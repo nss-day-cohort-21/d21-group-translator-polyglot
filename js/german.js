d711e2096255f45c8ad3e6861c641b7172c677ef
Michael Lindstrom <michaellindstr@gmail.com>
2017-07-25 15:35:37 -0500

    messed around but didn't change anything

--- js/german.js
+++ js/german.js
@@ -1,7 +1,14 @@
 'use strict';
 
 var German = (function(originalDic){
-  let germanLexicon = {"merry":"fröhlich", "christmas":"weihnachten", "and":"und", "happy":"frohes", "new":"neues", "year":"Jahr"}
+  let germanLexicon = {
+    "merry":"fröhlich",
+    "christmas":"weihnachten",
+    "and":"und",
+    "happy":"frohes",
+    "new":"neues",
+    "year":"Jahr"
+  }
 
   originalDic.getGerman = function(userInput){
     return germanLexicon
--- js/main.js
+++ js/main.js
@@ -15,4 +15,3 @@ translateBtn.addEventListener('click', function(event) {
         }
     });
 });
-
