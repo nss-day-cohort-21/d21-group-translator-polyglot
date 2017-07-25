9b37d415bc8aa7874440528cf221b5c00aeda296
Fang <funkapunkafide@gmail.com>
2017-07-25 14:34:25 -0500

    first commit

---
+++ js/french.js
@@ -0,0 +1,9 @@
+var Dictionary = (function(a){
+	let x = {"merry":"Joyeux", "christmas":"Noël", "and":"et", "happy":"content", "new":"Nouveau", "year":"an"};
+	let y = "merry";
+	return {
+		translatetofrench: function(x) {
+			
+		}
+	}
+})() || {};
