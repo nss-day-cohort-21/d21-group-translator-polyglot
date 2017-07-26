83f03d987e11305f1524275d6e86799b0ab5b06f
Michael Lindstrom <michaellindstr@gmail.com>
2017-07-26 08:58:06 -0500

    added speech

--- js/main.js
+++ js/main.js
@@ -3,25 +3,73 @@
 
 let options = Array.from(document.querySelectorAll('select'));
 let translateBtn = document.querySelector('#submit-button');
-let outPut = document.querySelector('#outPut');
+let input = document.querySelector('#input');
+let output = document.querySelector('#output');
 
 let getLanguage = function(language) {
-	let input = document.getElementById('input').value;
-    let inputArr = input.split(' ');
-    
+  console.log(language);
+	let textInput = input.value.toLowerCase().split(' ');
+
+  let translation = "";
+  textInput.forEach(function(word){
+    if(language[word]) {
+      translation += `${language[word]} `
+    } else {
+      language[word] = prompt(`We don't know that word? Do you know the definition of ${word}?`)
+    }
+
+  });
+
+  output.innerHTML = translation;
+  output.value = translation;
 }
 
 translateBtn.addEventListener('click', function(event) {
     options.forEach(function(option) {
         if (option.value === 'spanish') {
-        	console.log("option value", option.value);
             getLanguage(Dictionary.getSpanish());
         } else if (option.value === 'french') {
-			console.log("option value", option.value);
             getLanguage(Dictionary.translatetofrench());
         } else if (option.value === 'german') {
-			console.log("option value", option.value);
             getLanguage(Dictionary.getGerman());
+        } else if (option.value === 'braille') {
+          getLanguage(Dictionary.getBraille());
         }
     });
 });
+
+let voiceOptions = document.getElementById('voice');
+let volumeSlider = document.getElementById('volume');
+let rateSlider = document.getElementById('rate');
+let pitchSlider = document.getElementById('pitch');
+let speakButton = document.getElementById('speakButton');
+
+speakButton.addEventListener("click", speak)
+
+let voiceArr = [];
+
+function loadVoices () {
+  let voices = speechSynthesis.getVoices();
+  for (let i = 0; i < voices.length; i++) {
+    let voice = voices[i];
+    let option = document.createElement('option');
+    option.value = voice.name;
+    option.innerHTML = voice.name;
+    voiceOptions.appendChild(option);
+    voiceArr[voice.name] = voice;
+  };
+};
+
+window.speechSynthesis.onvoiceschanged = function(e){
+  loadVoices();
+};
+
+function speak () {
+  let translatedText = new SpeechSynthesisUtterance();
+  translatedText.volume = volumeSlider.value;
+  translatedText.voice = voiceArr[voiceOptions.value];
+  translatedText.rate = rateSlider.value;
+  translatedText.pitch = pitchSlider.value;
+  translatedText.text = output.value;
+  window.speechSynthesis.speak(translatedText);
+};
